b41493a003fdbfe6fed268618e1ecac73607996d
RobinKohrs <robin.kohrs@gmx.de>
2021-05-14 07:37:28 +0000

    back to sequential as parallel does not work

--- R/cumulated_pixel_value.R
+++ R/cumulated_pixel_value.R
@@ -6,36 +6,57 @@
 #' @export
 #'
 
-cumulate_pixel_value = function(
-  sm_image_paths = NULL
-){
-
+cumulate_pixel_value = function(sm_image_paths = NULL) {
   # check input
-  if(is.null(sm_image_paths)){
+  if (is.null(sm_image_paths)) {
     stop("You need to proivde character vector with paths to soilmoisture images")
   }
 
   # check paths
   t = all(grepl(".*soilmoisture.*\\.tif$$", sm_image_paths))
-  if(!t){
+  if (!t) {
     warning("I guess there might be problem with your paths")
   }
 
-  # calculate the cumulated pixel-values
-  registerDoFuture()
-  plan(multisession)
-  cumsum = foreach(i = seq_along(sm_image_paths),
-                   .combine = "+") %dopar%{
+  # # calculate the cumulated pixel-values
+  # registerDoFuture()
+  # plan(multisession)
+  # cumsum = foreach(i = seq_along(sm_image_paths),
+  #                  .combine = "+") %dopar%{
+  #
+  #                    star = read_stars(sm_image_paths[[i]])
+  #                    vals = star[[1]]
+  #                    vals
+  #                  }
+  #
+  #
+  # # read in the first image
+  # dummy = read_stars(sm_image_paths[[1]])
+  # dummy[[1]] = cumsum
 
-                     star = read_stars(sm_image_paths[[i]])
-                     vals = star[[1]]
-                     vals
-                   }
 
+  ######################################
+  # Do it in sequential as the parallel fails...
+  ######################################
+
+  sum = read_stars(sm_image_paths[[1]])[[1]]
+  for (i in seq_along(sm_image_paths)) {
+    if (i != 1) {
+      cat("\r", i, "/", length(sm_image_paths))
+      star = read_stars(sm_image_paths[[i]])
+      star_vals = star[[1]]
+      base_matrix = base_matrix + star_vals
+    }
+  }
 
   # read in the first image
   dummy = read_stars(sm_image_paths[[1]])
-  dummy[[1]] = cumsum
+  dummy[[1]] = sum
+
+
+  # return the dummy stars object with the computed values
+  return(dummy)
+
 
   # return the dummy stars object with the computed values
   return(dummy)
--- R/how_many_images_per_pixel.R
+++ R/how_many_images_per_pixel.R
@@ -14,21 +14,40 @@ how_many_images_per_pixel = function(
 ){
 
   # sum up the times it was not 0
-  registerDoFuture()
-  plan(multisession)
-  sum = foreach(i = seq_along(sm_image_paths),
-                .combine = "+") %dopar% {
+  # registerDoFuture()
+  # plan(multisession, workers = 6)
+  # sum = foreach(i = seq_along(sm_image_paths),
+  #               .combine = "+") %dopar% {
+  #
+  #                star = read_stars(sm_image_paths[[i]])
+  #                star_vals = star[[1]]
+  #                star_vals[star_vals == 0] = 0 # unnecessary
+  #                star_vals[star_vals > 0] = 1
+  #                star_vals
+  #               }
+  #
+  # # read in the first image
+  # dummy = read_stars(sm_image_paths[[1]])
+  # dummy[[1]] = sum
+  #
 
-                 star = read_stars(sm_image_paths[[i]])
-                 star_vals = star[[1]]
-                 star_vals[star_vals == 0] = 0 # unnecessary
-                 star_vals[star_vals > 0] = 1
-                 star_vals
-                }
+  sum = read_stars(sm_image_paths[[1]])[[1]]
+  for (i in seq_along(sm_image_paths)) {
+    if (i != 1) {
+      cat("\r", i, "/", length(sm_image_paths))
+      star = read_stars(sm_image_paths[[i]])
+      star_vals = star[[1]]
+      star_vals[star_vals == 0] = 0 # unnecessary
+      star_vals[star_vals > 0] = 1
+      base_matrix = base_matrix + star_vals
+    }
+  }
+
 
   # read in the first image
   dummy = read_stars(sm_image_paths[[1]])
   dummy[[1]] = sum
+
 
   # return the dummy stars object with the computed values
   return(dummy)
